885257d75ce645071e0a39f5fd8af5139f1b87ca
Damon Oehlman <damon.oehlman@gmail.com>
2024-11-23 16:29:54 +1100

    kitty

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -2,6 +2,10 @@
 {
   programs.kitty = {
     enable = true;
+    shellIntegration.enableZshIntegration = true;
     catppuccin.enable = true;
+    settings = {
+      backgroundOpacity = 0.9;
+    };
   };
 }
