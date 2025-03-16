dfee74b59273d4de50e9bc7e32bc77b606828663
Damon Oehlman <damon.oehlman@gmail.com>
2025-03-16 20:46:36 +1000

    .

--- hosts/modules/common-heavy.nix
+++ hosts/modules/common-heavy.nix
@@ -9,56 +9,6 @@
 
   # System packages
   environment.systemPackages = with pkgs; [
-    # nix
-    nil
-    nixfmt-classic
-    home-manager
-    nh
-
-    # languages
-    (python3.withPackages (ps: with ps; [ pip virtualenv ]))
-    pipenv
-    rustup
-    deno
-    nodejs_22
-    aiken
-
-    # development general
-    gnumake
-    binserve
-
-    # terminal
-    kitty
-
-    # editors
-    zed-editor
-    vscode
-    vim
-
-    # cli
-    pass
-    unzip
-    dig
-    git
-    delta
-    jq
-    killall
-    du-dust
-    cmake
-    fd
-    eza
-    ripgrep
-
-    # tuis
-    caligula
-
-    # chat
-    slack
-    discord
-
-    # music
-    spotify
-
     # cloud tooling
     awscli2
     terraform
@@ -69,13 +19,4 @@
     kubectl
     lazydocker
   ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.zed-mono
-    nerd-fonts.meslo-lg
-    roboto
-  ];
-
-  programs.zsh.enable = true;
 }
